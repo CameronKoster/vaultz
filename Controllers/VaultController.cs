a097ae244697e1aeb136130d781bb4e67526c69a
Cameron Koster <kostercam37@gmail.com>
2019-01-19 15:35:47 -0700

    vault controller and repo done. Moving to vaultkeep

--- Controllers/VaultController.cs
+++ Controllers/VaultController.cs
@@ -1,38 +1,58 @@
+using System.Collections.Generic;
+using Keepr.Models;
+using Keepr.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Keepr.Controllers
 {
+  [Route("api/[controller]")]
+  [ApiController]
 
   public class VaultsController : ControllerBase
   {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public VaultsController(Parameters) //constructor
+    //GetAll
+    [HttpGet]
+    public ActionResult<IEnumerable<Vault>> Get()
     {
+      return Ok(_vaultRepo.GetAllVaults());
+    }
 
+    //GetById
+    [HttpGet("{id}")]
+    public ActionResult<Vault> Get(int id)
+    {
+      Vault response = _vaultRepo.GetById(id);
+      if (response != null)
+      {
+        return Ok(response);
+      }
+      return BadRequest();
+    }
+
+    //AddVault
+    [HttpPost]
+    public ActionResult<Vault> Post([FromBody] Vault payload)  //want to add a string saying that you successfully added a Vault.
+    {
+      Vault response = _vaultRepo.AddVault(payload);
+      return Created("/api/Vault/" + response.Id, response);
+    }
+
+    //DeleteVault
+    [HttpDelete]
+    public ActionResult<string> Delete(int id)
+    {
+      if (_vaultRepo.DeleteVault(id))
+      {
+        return Ok("Successfully deleted!");
+      }
+      return BadRequest("Unable to delete. Try again later.");
+    }
+
+    //constructor
+    private readonly VaultRepository _vaultRepo;
+    public VaultsController(VaultRepository vaultRepo)
+    {
+      _vaultRepo = vaultRepo;
     }
   }
 }
--- Repositories/VaultRepository.cs
+++ Repositories/VaultRepository.cs
@@ -1,33 +1,46 @@
-
-
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using Keepr.Models;
 
 namespace Keepr.Repositories
 {
 
   public class VaultRepository
   {
+    //GetAll
+    public IEnumerable<Vault> GetAllVaults()
+    {
+      return _db.Query<Vault>("SELECT * FROM Vault");
+    }
 
+    //GetById
+    public Vault GetById(int id)
+    {
+      return _db.QueryFirstOrDefault<Vault>($"SELECT * FROM Vault WHERE id = @id", new { id });
+    }
 
+    //AddVault
+    public Vault AddVault(Vault newVault)
+    {
+      int id = _db.ExecuteScalar<int>(@"
+     INSERT INTO Vault(name,description,userId)
+     VALUES(@Name,@Description,@UserId);
+     SELECT LAST_INSERT_ID();
+     ", newVault);
+      newVault.Id = id;
+      return newVault;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private readonly IDbConnection _db
-    public VaultRepository(int IDbConnection)
+    //DeleteVault
+    public bool DeleteVault(int id)
+    {
+      int successfullyDeleted = _db.Execute(@"DELETE FROM Vault WHERE id = @id", new { id });
+      return successfullyDeleted != 0;
+    }
+    //contructor
+    private readonly IDbConnection _db;
+    public VaultRepository(IDbConnection db)
     {
       _db = db;
     }

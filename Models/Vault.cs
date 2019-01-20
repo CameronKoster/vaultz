2dc23031036e02a9a2bbd7832dae58fdd992696b
Cameron Koster <kostercam37@gmail.com>
2019-01-19 18:42:06 -0700

    working on registering an account to test routes

--- Models/Vault.cs
+++ Models/Vault.cs
@@ -4,7 +4,7 @@ namespace Keepr.Models
   {
     public int Id { get; set; }
     public string Name { get; set; }
-    public int Description { get; set; }
+    public string Description { get; set; }
     public int UserID { get; set; }
 
 
